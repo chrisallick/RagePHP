8c788798c552aa6ca25dfdc801bf2928fc687c9a
Chris Allick <chrisallick@gmail.com>
2015-03-31 12:19:39 -0400

    cached videos

--- videos.php
+++ videos.php
@@ -3,7 +3,8 @@
     $keys = array();
     $videos = array();
 
-    $spreadsheet_url="https://docs.google.com/spreadsheet/pub?key=0AqxsTh_PqRW4dENLanpFa2lXV1NsV2hNMUF0ay1Sd2c&gid=0&output=csv";
+    //$spreadsheet_url="https://docs.google.com/spreadsheet/pub?key=0AqxsTh_PqRW4dENLanpFa2lXV1NsV2hNMUF0ay1Sd2c&gid=0&output=csv";
+    $spreadsheet_url = "./videos.csv";
 
     if(!ini_set('default_socket_timeout',    15)) echo "<!-- unable to change socket timeout -->";
 
@@ -28,4 +29,25 @@
 
     // print_r( $raw );
     // print_r( $videos );
+
+    // $news = simplexml_load_file('https://spreadsheets.google.com/feeds/list/0AqxsTh_PqRW4dENLanpFa2lXV1NsV2hNMUF0ay1Sd2c/od6/public/basic?alt=rss');
+
+    // $feeds = array();
+
+    // $i = 0;
+
+    // foreach ($news->channel->item as $item) {
+    //     preg_match('@src="([^"]+)"@', $item->description, $match);
+    //     $parts = explode('<font size="-1">', $item->description);
+
+    //     $feeds[$i]['title'] = (string) $item->title;
+    //     $feeds[$i]['category'] = (string) $item->category;
+    //     $feeds[$i]['description'] = (string) $item->description;
+
+    //     $i++;
+    // }
+
+    // echo '<pre>';
+    // print_r($feeds);
+    // echo '</pre>';
 ?>
